d932f41c29d0213c2a580499397ecf0df0d12240
Sai Harshith Reddy <saiharshithreddy383@gmail.com>
2025-12-04 13:33:20 +0530

    Inital commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('CI/CD Pipeline Working Successfully!');
+});
+app.listen(3000, () => console.log('Server Running on 3000'));
